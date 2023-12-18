115a8b1b6f19702e9f7273d001422a9afeb879d8
Mani <154072384+usmanmani584@users.noreply.github.com>
2023-12-18 23:19:47 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923175695584', '—͟͞͞★ოąꪀꭵ—͟͞͞★', true],
+  ['923200035701', '𝐑𝐞𝐡𝐦𝐚𝐧𝐢 𝐃𝐢𝐠𝐢𝐭𝐚𝐥', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.mods = ['923175695584','923200035701'] 
+global.prems = ['923175695584', '923200035701', '919398758484']
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
